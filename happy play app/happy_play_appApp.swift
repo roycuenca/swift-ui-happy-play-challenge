639889fac71c611ddfcc4a134f11293a186c67d4
roycuenca <roycuenca2013@gmail.com>
2023-07-11 18:25:16 -0300

    Initial Commit

---
+++ happy play app/happy_play_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  happy_play_appApp.swift
+//  happy play app
+//
+//  Created by Roy Cuenca on 11/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct happy_play_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
